50ce54561c466999abbfff3e0af94cfdabc7a6e1
RispaJoseph <rizpahjoseph@gmail.com>
2026-01-26 15:21:38 +0530

    scraping country name and their links

--- spider_tutorial/spider_tutorial/spiders/worldometers.py
+++ spider_tutorial/spider_tutorial/spiders/worldometers.py
@@ -7,10 +7,16 @@ class WorldometersSpider(scrapy.Spider):
     start_urls = ["https://www.worldometers.info/world-population/population-by-country"]
 
     def parse(self, response):
-        title = response.xpath('//h1/text()').get()
-        countries = response.xpath('//td/a/text()').getall()
         
-        yield {
-            'titles': title,
-            'counties': countries,
-        }
+        # title = response.xpath('//h1/text()').get()
+        countries = response.xpath('//td/a')
+        
+        for country in countries:
+            country_name = country.xpath('.//text()').get()
+            link = country.xpath('.//@href').get()
+            
+        
+            yield {
+                'country-name': country_name,
+                'link': link,
+            }
