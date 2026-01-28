edac51451e1519e415b84acfbad907489272565c
RispaJoseph <rizpahjoseph@gmail.com>
2026-01-28 13:05:35 +0530

    Api - working with pagination

--- api_project/api_project/spiders/quotes.py
+++ api_project/api_project/spiders/quotes.py
@@ -16,3 +16,10 @@ class QuotesSpider(scrapy.Spider):
                 'tags': quote.get('tags'),
                 'quotes': quote.get('text'),
             }
+        has_next = json_response.get('has_next')
+        if has_next:
+            next_page = json_response.get('page')+1
+            yield scrapy.Request(
+                url = f'https://quotes.toscrape.com/api/quotes?page={next_page}',
+                callback= self.parse
+            )
